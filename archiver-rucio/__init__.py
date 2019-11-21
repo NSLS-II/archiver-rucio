8f3ea94e798b274212a2af3d0f61d6be119bdf65
Garrett Bischof <gbischof@bnl.gov>
2019-11-21 16:57:15 -0500

    trying to work out how to register a file to rucio

--- archiver-rucio/__init__.py
+++ archiver-rucio/__init__.py
@@ -1,4 +1,30 @@
+import os
+import suitcase.jsonl
+import suitcase.msgpack
 
 from ._version import get_versions
+
 __version__ = get_versions()['version']
 del get_versions
+
+class Archiver():
+    """
+    Serialize bluesky documents and register them with Rucio.
+    """
+    def __init__(self, rucio_client, suitcase_class, **kwargs):
+        archivable = (suitcase.jsonl.Serializer, suitcase.msgpack.Serializer):
+
+        if suitcase_class not in archivable:
+            raise TypeError(f"suitcase_class not in {archivable}")
+
+        self.suitcase = suitcase_class(**kwargs)
+        self.rucio_client = rucio_client
+
+    def __call__(self, name, doc):
+        self.suitcase(name, doc)
+        if name == 'start':
+            self.register()
+
+    def register(self):
+        filename = os.path.realpath(self.suitcase._outputfile.name)
+
