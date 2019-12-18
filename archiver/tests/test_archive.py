8f42b497752f33c7a958599c3f006f8bb9b2db52
Garrett Bischof <gbischof@bnl.gov>
2019-12-18 18:24:57 +0000

    whitespace

--- archiver/tests/test_archive.py
+++ archiver/tests/test_archive.py
@@ -2,7 +2,6 @@ from .. import Archiver
 
 def test_export(example_data):
     # Exercise the archiver on the myriad cases parametrized in example_data.
-	
     archiver = Archiver(directory='/home/vagrant/globus')
     documents = example_data()
     for name, doc in documents:
